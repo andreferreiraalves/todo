a4eb9747aafb51b16b7ff3fbb333756e43cb117e
Andre Ferreira <andre.ferreira@mercatustecnologia.com.br>
2019-11-28 12:03:07 -0300

    adicionando estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +8,67 @@ import { Todo } from 'src/models/todo.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: Todo[] = [];
+  public title = 'Minhas Tarefas';
 
-  constructor() {
-    this.todos.push(new Todo(1, 'task 1', false));
-    this.todos.push(new Todo(2, 'task 2', false));
-    this.todos.push(new Todo(3, 'task 3', true));
+  public todos: Todo[] = [];
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.load();
+  }
+
+  add() {
+    // this.form.value => { title : 'Título' }
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
-    if (index !== -1)
+    if (index !== -1) {
       this.todos.splice(index, 1);
+      this.save();
+    }
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = JSON.parse(localStorage.getItem('todos'));
+
+    if (data)
+      this.todos = data;
+    else
+      this.todos = [];
+  }
 }
